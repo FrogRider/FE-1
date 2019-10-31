b76d56a9a22615e539d28cd861d1d73f4acd2b98
dkashmensky <d.kashmensky@gmail.com>
2019-10-31 17:30:06 +0200

    Add slider on the main page

---
+++ html/Task3/script/slider.js
@@ -0,0 +1,33 @@
+/*jshint esversion: 6 */
+// By Dmitriy Kashmenskiy
+// Simple slider for HTML Task 3 project
+
+$(document).ready(function() {
+  let slideIndex = 2;
+  setTimeout(initSlider, 6000);
+
+  function removeSlide() {
+    $('.slider__item')[0].remove();
+  }
+
+  function initSlider() {
+    // add new slider element
+    $('.slider').append(`
+      <div class="slider__item slider__item--new"><img src="img/pumpkin-${slideIndex}.jpg" alt=""></div>
+    `);
+
+    // animate new slide so it enters the page from the left
+    $('.slider__item--new').animate({
+      left: '0'
+    }, 2000);
+
+    slideIndex++;
+    if(slideIndex > 3) {
+      slideIndex = 1;
+    }
+
+    // remove previous slide and loop the slider
+    setTimeout(removeSlide, 2000);
+    setTimeout(initSlider, 6000);
+  }
+});
